e8dd121200adcce42749d20fdbffe8e003a6e059
Arlo Ihrig <arlo@radfx.com>
2023-10-27 14:11:08 -0700

    Add additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import prisma from '@/prisma/client';
-import { notFound } from 'next/navigation';
-import { Card, Flex, Heading } from '@radix-ui/themes';
-import IssuesPage from '../page';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import prisma from '@/prisma/client';
+import { Card, Flex, Heading } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 
 interface Props {
