2bdd522e44b743aad996013ed8a398c04a47d7a2
Arlo Ihrig <arlo@radfx.com>
2023-10-27 19:36:27 -0700

    Organize imports, refactor Skeleton

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,12 @@
-import { Table } from '@radix-ui/themes';
-import Link from '../components/Link';
 import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
+import { Table } from '@radix-ui/themes';
+import { Link, IssueStatusBadge } from '@/app/components';
 import IssueActions from './IssueActions';
 
 type Status = 'OPEN' | 'IN_PROGRESS' | 'CLOSED';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
