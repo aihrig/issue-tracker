a47102f5ea220931eeccc6bff7939da1654f25cb
Arlo Ihrig <arlo@radfx.com>
2023-10-29 13:54:35 -0700

    Handle updating issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -35,7 +35,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setIsSubmitting(true);
-      await axios.post('/api/issues', data);
+      if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
+      else await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
       setError('An unexpected error ocurred');
@@ -69,7 +70,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? 'Update ' : 'Submit New '} Issue{' '}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
